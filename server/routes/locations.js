b77f45f4842a2977fa54f2793238c9b0a2243a1b
ethwinch <ethwinch@gmail.com>
2026-06-13 19:39:36 -0400

    Add files via upload

---
+++ server/routes/locations.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import path from 'path'
+import LocationsController from '../controllers/locations.js'
+
+import { fileURLToPath } from 'url'
+
+// import locationsData from '../data/locations.js'
+
+const __filename = fileURLToPath(import.meta.url) //import.meta.url -> contains the URL of the current module file
+const __dirname = path.dirname(__filename)
+
+const router = express.Router()
+
+router.get('/', LocationsController.getLocations)
+
+router.get('/:locationID', (req, res) => {
+    console.log('Location route hit: ', req.params.locationID)
+    res.status(200).sendFile(path.resolve(__dirname, '../public/location.html'))
+})
+
+
+export default router
---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import './config/dotenv.js'
+import locationsRouter from './routes/locations.js'
+
+const app = express()
+
+// initialize middleware
+app.use('/public', express.static('./public'))
+app.use('/scripts', express.static('./public/scripts'))
+app.use('/locations', locationsRouter)
+
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+    res.status(200).send('<h1>LISTICLE PROJECT P1</h1>')
+})
+
+
+// Start server on Port 3001
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`)
+})
+
+
